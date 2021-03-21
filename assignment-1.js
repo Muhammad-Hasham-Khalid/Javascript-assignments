c7c1bfcde1eb3ecb6363b14ece10b609b81ae3fa
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2021-03-21 14:30:35 +0500

    new: upload assignment 1

---
+++ assignment-1.js
@@ -0,0 +1,36 @@
+// Create variables and store some data for different data types in them and print their values on the console
+
+// Q: Comment which of the following are valid variable names and which are not:
+// name       ()
+// $name      ()
+// _name      ()
+// ?name      ()
+// 1name      ()
+// name1      ()
+// name_1     ()
+// name-1     ()
+// n1a2m3e4   ()
+// NAME       ()
+
+// Q: Write what will be printed to the console after running the following code:
+/*
+  console.log(a);
+  var a;
+  a = 5;
+*/
+// Answer:
+
+// Q: Create two variables num1 and num2 and take their values input from the user and perform all the basic arithmetic operations (+,-,*,/,%) on them
+
+// Q: Take a number input from the user and show and alert whether the number is even or not
+
+// Q: Replace '?' with the expected values of the variables in each step in the followuing code
+/*
+  var a = 5;           (a=?, b=?, c=?)
+  var b = a++;         (a=?, b=?, c=?)
+  var c = ++a;         (a=?, b=?, c=?)
+
+  console.log(a);      (a=?, b=?, c=?)
+  console.log(b);      (a=?, b=?, c=?)
+  console.log(c);      (a=?, b=?, c=?)
+*/
