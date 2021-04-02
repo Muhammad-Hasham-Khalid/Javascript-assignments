e6150bfba2f9b94200fa425e770e9b9c8bc6f7e7
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2021-04-03 00:46:12 +0500

    new: Add assignment 2 and 3

---
+++ assignment-2.js
@@ -0,0 +1,35 @@
+// Given 2 strings, create a new string by concatenating the original string using the '+' operator and the concat method
+const string1 = 'Hello'
+const string2 = ' World'
+
+
+// Given a string, using loops print each character of the string on a new line
+const loopsWithString = 'Pakistan'
+
+// How you will get the last character from a string
+// let string = 'Hello World'
+// Answer: 
+
+// What is the equivalent method for the index operator '[]' to get the character of a certain position from a string
+
+// Given the following string:
+// let string = 'Pakistan'
+// How you will get the ascii code of the letter at the index 4
+
+// Given an array of filenames, iterate over the array & print true on the console if the file is a txt file
+const files = ['document.txt', 'hello.docx', 'names.xlsx', 'profile.txt', 'sample.ppt']
+
+// Take an string as input from the user and print whether the string contains a vowel or not and also tell at which index the vowel occurs at first
+
+// Given an string replace the character 'a' with 'e' and 'll' with 'oo'
+const stringToChange = 'Hello World as Javascript';
+
+// Given an string like this -> '12:00:00'
+// Extract out the hrs, minutes and seconds from it.
+
+// Take an string as input from user and also take a number as count and repeat the entered string count times and alert it
+// Example:
+// Entered string: 'Hello'
+// Entered count : 3
+// Result        : 'HelloHelloHello'
+// Note: Use the builtin method to implement it
---
+++ assignment-3.js
@@ -0,0 +1,29 @@
+// Create a function named 'logNameAndAge' and pass name and age as params and log them inside the function so that the following code should work
+
+logNameAndAge('Hasham', 20)
+logNameAndAge('John', 33)
+
+// Create a function to add, subtract, divide, multiply 2 numbers based on the numbers and the operation passed as the parameter
+// the following code should work afterwards
+
+calculate(1, 2, '+')
+calculate(5, 9, '*')
+calculate(22, 8, '-')
+calculate(17, 3, '/')
+
+// Write what will happen in the after running the following code and also explain why.
+
+/*
+someFunction();
+anotherFunction();
+
+const someFunction = function () {
+  console.log('Function was called!')
+}
+
+function anotherFunction() {
+  console.log('another function was called!')
+}
+*/
+
+// Answer:
